9f5cde24238ca3ec0e92c4d8facc53471f7c3d9c
Willem <w.meijer.5@student.rug.nl>
2023-10-23 09:47:53 +0000

    minor aggregator improvement

--- custom_services/gateway_aggregator_service/gateway_aggregator_service/aggregator.py
+++ custom_services/gateway_aggregator_service/gateway_aggregator_service/aggregator.py
@@ -5,16 +5,20 @@ Implements simple message aggregator.
 from typing import List, Any
 import asyncio
 import aiohttp
-from fastapi import Request
+from fastapi import Request, HTTPException
+
+
+BASE_ENDPOINT_HEADER_KEY = "X-BaseEndpoint"
+AGGREGATED_ENDPOINT_HEADER_KEY = "X-AggregatedEndpoints"
 
 
 async def aggregate_requests(request: Request) -> List[bytes]:
     """Performs the requests in parallel and aggregates their results."""
-    # TODO: if this fails, raise something to trigger a 400 HTTP error.
-    base_endpoint = get_base_endpoint(request)
-    target_endpoints = get_aggregated_endpoints(request)
-    # TODO: update this to work with a formatted string instead
-    # (i.e., that the endpoint can be specified somewhere inside the URL, not just at the end.)
+    try:
+        base_endpoint = get_base_endpoint(request)
+        target_endpoints = get_aggregated_endpoints(request)
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=f"Invalid headers: {ex.__cause__}.")
     target_urls = [
         f"{base_endpoint}{target_endpoint}" for target_endpoint in target_endpoints
     ]
@@ -24,13 +28,19 @@ async def aggregate_requests(request: Request) -> List[bytes]:
 
 def get_base_endpoint(request: Request) -> str:
     """Returns the base endpoint for the request."""
-    return request.headers.get("X-BaseEndpoint")
+    base = request.headers.get(BASE_ENDPOINT_HEADER_KEY)
+    if base is None:
+        raise KeyError(f'Header "{BASE_ENDPOINT_HEADER_KEY}" is missing.')
+    return base
 
 
 def get_aggregated_endpoints(request: Request) -> List[str]:
     """Extracts aggregated endpoints from the provided request."""
-    aggregate_endpoints = request.headers.get("X-AggregatedEndpoints")
-    return aggregate_endpoints.split(",")
+    aggregate_endpoints = request.headers.get(AGGREGATED_ENDPOINT_HEADER_KEY)
+    if aggregate_endpoints is None:
+        raise KeyError(f'Header "{AGGREGATED_ENDPOINT_HEADER_KEY}" is missing.')
+    endpoints = aggregate_endpoints.split(",")
+    return endpoints
 
 
 async def get_many(urls: List[str]) -> List[Any]:
