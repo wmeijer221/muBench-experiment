d087d861feb79afd16c6de02f4f270499689cc68
Willem <w.meijer.5@student.rug.nl>
2023-11-03 08:23:46 +0000

    fix file reference error

--- gssi_experiment/util/run-full-experiment.sh
+++ gssi_experiment/util/run-full-experiment.sh
@@ -6,14 +6,14 @@
 # TODO: Fix this hardcoded stuff.
 cd ./gssi_experiment/gateway_aggregator/gateway_aggregator_service
 make refresh
-cd ../.. 
+cd ../../..
 
 
 echo Running experiment with topology: \"$1\"
-python3 Deployers/K8sDeployer/RunK8sDeployer.py -c $1 -y -r
+python3 ./Deployers/K8sDeployer/RunK8sDeployer.py -c $1 -y -r
 
 echo Waiting $3 seconds for pods to start.
 sleep $3
 
 echo Running experiment with load: \"$2\"
-python3 Benchmarks/Runner/Runner.py -c $2
+python3 ./Benchmarks/Runner/Runner.py -c $2
