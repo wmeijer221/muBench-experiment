ce200198bfd42656c0b8cec8f21beb893b7c04e4
Willem <w.meijer.5@student.rug.nl>
2023-11-15 08:37:12 +0000

    bettr many experiments

--- gssi_experiment/gateway_aggregator/many-experiments.sh
+++ gssi_experiment/gateway_aggregator/many-experiments.sh
@@ -1,5 +1,13 @@
-DELAY=5
+# Configurations of big experiment #1: workload=15000, trials=25 (effectively randomly chosen).
+# Configurations of big experiment #2: workload=10000, trials=17 (chosen and reduce time to emphasize vCPU=1, respectively).
+
+
+DELAY=60
 BIG_NODE=node-3
+WORKLOAD=10000
+# This is 17 because at trials=25 the most accurate model lay at vCPU limit=1500m
+# i.e., if we want to model for 1 vCPU, the load should be reduced by 1/3; i.e. to trials~17.
+TRIALS=17
 
 let counter=1
 
@@ -10,12 +18,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 1000m \
         --replicas 1 \
-        --name "1000m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/1000m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -23,12 +31,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 750m \
         --replicas 1 \
-        --name "750m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/750m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -36,12 +44,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 1250m \
         --replicas 1 \
-        --name "1250m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/1250m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -49,12 +57,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 500m \
         --replicas 1 \
-        --name "500m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/500m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -62,12 +70,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 1500m \
         --replicas 1 \
-        --name "1500m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/1500m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -75,12 +83,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 1750m \
         --replicas 1 \
-        --name "1750m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/1750m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -88,12 +96,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 2000m \
         --replicas 1 \
-        --name "2000m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/2000m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -101,12 +109,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 2500m \
         --replicas 1 \
-        --name "2500m_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/2500m_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -114,12 +122,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method node_selector \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit '' \
         --replicas 1 \
-        --name "no_cpu_cap_1rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/no_cpu_cap_1rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -127,12 +135,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method equal_distribution \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 1000m \
         --replicas 2 \
-        --name "1000m_2rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/1000m_2rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -140,12 +148,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method equal_distribution \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit 1000m \
         --replicas 3 \
-        --name "1000m_3rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/1000m_3rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -153,12 +161,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method equal_distribution \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit '' \
         --replicas 2 \
-        --name "no_cpu_cap_2rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/no_cpu_cap_2rep_20trials/run_$VARIABLE"
     let counter++
 
     python3 ./gssi_experiment/gateway_aggregator/service-intensity-experiment.py \
@@ -166,12 +174,12 @@ do
         --node-selector $BIG_NODE \
         --node-selector-method equal_distribution \
         --steps 25 \
-        --trials 25 \
+        --trials $TRIALS \
         --seed $counter \
-        --workload-events 15000 \
+        --workload-events $WORKLOAD \
         --cpu-limit '' \
         --replicas 3 \
-        --name "no_cpu_cap_3rep_25trials/run_$VARIABLE"
+        --name "large_experiment_2/no_cpu_cap_3rep_20trials/run_$VARIABLE"
     let counter++
 
 done
