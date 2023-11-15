953574927828a3c5c359baf80597b09e108b61d8
Willem <w.meijer.5@student.rug.nl>
2023-11-15 10:00:53 +0000

    sleep after deleting deployment

--- gssi_experiment/util/experiment_helper.py
+++ gssi_experiment/util/experiment_helper.py
@@ -215,8 +215,17 @@ def _rewrite_mubench_results(input_path: str, output_path: str):
                 csv_writer.writerow(data_point)
 
 
-def apply_k8s_yaml_file(file_path: str):
+def apply_k8s_yaml_file(file_path: str, sleep_between_reapply: int = 30):
     """Applies a yaml field using kubectl"""
+    # Deletes old deployment
+    args = ['kubectl', 'delete', '-f', file_path]
+    proc = Popen(args)
+    statuscode = proc.wait()
+    if statuscode != 0:
+        print(f'Could not delete "{file_path}".')
+    print(f'Sleeping {sleep_between_reapply} seconds before reapplying "{file_path}".')
+    sleep(sleep_between_reapply)
+    # Applies the new one.
     args = ["kubectl", "apply", "-f", file_path]
     proc = Popen(args)
     statuscode = proc.wait()
