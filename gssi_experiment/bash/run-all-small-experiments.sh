775d45d9cea16c41a769d87897b0a508a04a571f
Willem <w.meijer.5@student.rug.nl>
2023-11-20 13:12:02 +0000

    run all experiments script!

---
+++ gssi_experiment/bash/run-all-small-experiments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+SLEEP_TIME=120
+
+./gssi_experiment/gateway_offloading/experiment-small.sh
+sleep $SLEEP_TIME
+
+./gssi_experiment/pipes_and_filters/pipes_and_filters_joint/experiment-small.sh
+sleep $SLEEP_TIME
+
+./gssi_experiment/pipes_and_filters/pipes_and_filters_separated/experiment-small.sh
+sleep $SLEEP_TIME
+
+# This should probably not be run earlier as the aggregator
+# service isn't automatically deleted yet.
+./gssi_experiment/gateway_aggregator/experiment-small.sh
+sleep $SLEEP_TIME
