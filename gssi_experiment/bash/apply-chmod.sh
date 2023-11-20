793c1f154c72c4a9e78e3e41535e839d22724166
Willem <w.meijer.5@student.rug.nl>
2023-11-20 17:37:54 +0000

    integrated chmod

--- gssi_experiment/bash/apply-chmod.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-chmod +x gssi_experiment/bash/run-all-small-experiments.sh
-chmod +x gssi_experiment/gateway_aggregator/experiment-small.sh
-chmod +x gssi_experiment/gateway_offloading/experiment-small.sh
-chmod +x gssi_experiment/pipes_and_filters/pipes_and_filters_joint/experiment-small.sh
-chmod +x gssi_experiment/pipes_and_filters/pipes_and_filters_separated/experiment-small.sh
--- gssi_experiment/bash/run-all-small-experiments.sh
+++ gssi_experiment/bash/run-all-small-experiments.sh
@@ -2,16 +2,20 @@
 
 SLEEP_TIME=120
 
+chmod +x gssi_experiment/pipes_and_filters/pipes_and_filters_joint/experiment-small.sh
 ./gssi_experiment/pipes_and_filters/pipes_and_filters_joint/experiment-small.sh
 sleep $SLEEP_TIME
 
+chmod +x gssi_experiment/pipes_and_filters/pipes_and_filters_separated/experiment-small.sh
 ./gssi_experiment/pipes_and_filters/pipes_and_filters_separated/experiment-small.sh
 sleep $SLEEP_TIME
 
+chmod +x gssi_experiment/gateway_offloading/experiment-small.sh
 ./gssi_experiment/gateway_offloading/experiment-small.sh
 sleep $SLEEP_TIME
 
 # This should probably not be run before the others as the
 # aggregator service isn't automatically deleted yet.
+chmod +x gssi_experiment/gateway_aggregator/experiment-small.sh
 ./gssi_experiment/gateway_aggregator/experiment-small.sh
 sleep $SLEEP_TIME
