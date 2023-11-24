59d17d140c1401283abe237f6824ef18508c7536
Willem <w.meijer.5@student.rug.nl>
2023-11-24 14:36:13 +0000

    update to include std in plots

---
+++ gssi_experiment/util/notebook_helper.py
@@ -0,0 +1,125 @@
+import typing
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def to_averaged_df(
+    df: pd.DataFrame,
+    group_on: "str | typing.Iterator[str]",
+    averaged_columns: typing.Iterator[str],
+) -> pd.DataFrame:
+    if isinstance(group_on, str):
+        group_on = [group_on]
+    # averaged_columns = ["avg_latency_ms", *cpu_columns, *synth_cpu_cols]
+    print(f"{averaged_columns=}")
+    grp_df: pd.DataFrame = df.select_dtypes(include="number")
+    grp_df = grp_df.groupby(group_on)
+
+    avg_df: pd.DataFrame = grp_df.mean(averaged_columns)
+
+    std_df: pd.DataFrame = grp_df.std()
+    std_columns = [f"std_{col}" for col in std_df.columns if not col in group_on]
+    std_df.columns = std_columns
+    avg_df = avg_df.merge(std_df, on=group_on)
+
+    if len(group_on) > 1:
+        for i, key in enumerate(group_on):
+            avg_df[key] = [val[i] for val in avg_df.index]
+    else:
+        avg_df[group_on[0]] = avg_df.index
+
+    avg_df = avg_df.reset_index(drop=True)
+
+    return avg_df
+
+
+def create_figure(df: pd.DataFrame, x_key: str, synth_key: str, real_key: str):
+    # Create a figure and axis
+    plt.figure()
+    ax = plt.axes()
+
+    # Plot the first line (sine function)
+    ax.plot(df[x_key], df[synth_key], label="Theoretical", color="blue")
+
+    # Plot the second line (cosine function)
+    ax.plot(df[x_key], df[real_key], label="Experimental", color="red")
+    upper = df[real_key] + df[f"std_{real_key}"]
+    lower = df[real_key] - df[f"std_{real_key}"]
+    ax.fill_between(df[x_key], upper, lower, facecolor="red", alpha=0.25)
+
+    # Add labels and a legend
+    ax.set_ylabel("Latency (ms)")
+    ax.set_xlabel(x_key)
+    ax.set_title("Theoretical and experimental results for response latency")
+    ax.legend()
+
+    # Show the plot
+    plt.show()
+
+
+def create_multi_figure(
+    df: pd.DataFrame, y_keys: typing.List[str], x_key: str, split_key: str, y_label: str
+):
+    splits = df[split_key].unique()
+    splits.sort()
+    num_subplots = len(splits)
+
+    num_rows = num_subplots // 2
+    num_cols = 2
+    _, axes = plt.subplots(num_rows, num_cols, figsize=(12, 12))
+    axes = axes.flatten() if num_rows > 1 else [axes]
+
+    for i, split in enumerate(splits):
+        tmp_df: pd.DataFrame = df[df[split_key] == split]
+        ax: plt.Axes = axes[i]
+
+        tmp_df = tmp_df.sort_values(x_key)
+        for y_key in y_keys:
+            tmp_df.plot(x_key, y_key, ax=ax)
+            std_key = f"std_{y_key}"
+            upper = tmp_df[y_key] + tmp_df[std_key]
+            lower = tmp_df[y_key] - tmp_df[std_key]
+            ax.fill_between(tmp_df[x_key], upper, lower, alpha=0.4)
+
+        ax.set_xlabel(x_key)
+        ax.set_ylabel(y_label)
+        ax.set_title(f"{split_key}={split}")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def create_plot_comparisons(
+    comparison_tuples: typing.List[typing.Tuple[str]], df: pd.DataFrame, x_column: str
+):
+    # Calculate the number of subplots based on the length of comparison_tuples
+    num_subplots = len(comparison_tuples)
+    # Determine the number of rows and columns for the subplots
+    num_rows = num_subplots // 2  # Assuming 2 columns
+    num_cols = 2  # Number of columns for the subplots
+
+    # Create a larger figure with subplots
+    _, axes = plt.subplots(num_rows, num_cols, figsize=(8, 8))
+
+    # Flatten the axes array if there is more than one row
+    axes = axes.flatten() if num_rows > 1 else [axes]
+
+    for i, columns in enumerate(comparison_tuples):
+        # Select the current subplot
+        ax: plt.Axes = axes[i]
+
+        # Plot a line diagram for each pair of columns in the DataFrame
+        for column in columns:
+            df.plot(x=x_column, y=column, ax=ax)
+            std_key = f"std_{column}"
+            upper = df[column] + df[std_key]
+            lower = df[column] - df[std_key]
+            ax.fill_between(df[x_column], upper, lower, alpha=0.4)
+
+        ax.set_xlabel("Dasbhoard Intensity")
+        ax.set_ylabel("CPU utilization")
+
+    # Adjust layout to prevent overlapping titles
+    plt.tight_layout()
+    plt.show()
